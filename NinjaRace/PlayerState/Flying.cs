84a08e323824cc62e3350cb84c886bb837497521
Shadeoflance <shadeoflance@gmail.com>
2014-06-27 01:41:18 +0400

    ignore

--- NinjaRace/PlayerState/Flying.cs
+++ NinjaRace/PlayerState/Flying.cs
@@ -4,12 +4,12 @@ using System;
 
 class Flying : PlayerState
 {
-    double Gravity = 5000, Acc = 1200;
+    double Gravity = 6000, GAcc = 1200;
     public Flying(Player player) : base(player) { }
     public override void Update(double dt)
     {
         Player.Velocity -= Vec2.Clamp(new Vec2(Player.Velocity.X - Player.Controller.NeedVel().X * Speed, 0), Acc * dt);
-        Player.Velocity -= Vec2.Clamp(new Vec2(0, Player.Velocity.Y + Gravity), Acc * dt);
+        Player.Velocity -= Vec2.Clamp(new Vec2(0, Player.Velocity.Y + Gravity), GAcc * dt);
     }
     public override void Render()
     {
