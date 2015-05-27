f176191237b9132ecfa2d30b54a1951287f6c1d9
Shadeoflance <shadeoflance@gmail.com>
2015-05-28 00:16:30 +0300

    DropTile improvements

--- NinjaRace/Level/Tile/impl/DropTile.cs
+++ NinjaRace/Level/Tile/impl/DropTile.cs
@@ -1,9 +1,12 @@
 ﻿using VitPro;
 using VitPro.Engine;
 using System;
+using System.Collections.Generic;
 
 class DropTile : Tile
 {
+    List<Tile> near = null;
+
     public DropTile()
     {
         Moving = true;
@@ -19,6 +22,24 @@ class DropTile : Tile
         {
             dropping = true;
             this.player = player;
+            if (near == null)
+            {
+                near = new List<Tile>();
+                Vec2i coords = Tiles.GetCoords(ID);
+                int x = coords.X + 1;
+                Tiles tiles = Program.World.level.tiles;
+                while (tiles.GetTile(x, coords.Y) is DropTile)
+                {
+                    near.Add(tiles.GetTile(x, coords.Y));
+                    x++;
+                }
+                x = coords.X - 1;
+                while (tiles.GetTile(x, coords.Y) is DropTile)
+                {
+                    near.Add(tiles.GetTile(x, coords.Y));
+                    x--;
+                }
+            }
         }
     }
 
@@ -26,6 +47,11 @@ class DropTile : Tile
     {
         if (!dropping)
             return;
+        if (player.States.current is Flying)
+        {
+            dropping = false;
+            return;
+        }
         foreach (var a in player.collisions[Side.Down])
             if (!a.IsMark && !(a is DropTile))
             {
@@ -34,6 +60,12 @@ class DropTile : Tile
             }
         Vec2 v = new Vec2(0, dt * speed);
         Position -= v;
+        foreach (var a in near)
+        {
+            if (player.collisions[Side.Down].Contains(a))
+                continue;
+            a.Position = new Vec2(a.Position.X, Position.Y);
+        }
         player.Position = new Vec2(player.Position.X, Position.Y + Size.Y + player.Size.Y);
         dropping = player.collisions[Side.Down].Contains(this);
     }
