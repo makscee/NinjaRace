0b13f4570d7f10a012d690c64f30d30338af00ad
Shadeoflance <shadeoflance@gmail.com>
2015-07-18 15:27:57 +0300

    Death animation

--- NinjaRace/Player/States/impl/Dead.cs
+++ NinjaRace/Player/States/impl/Dead.cs
@@ -4,13 +4,36 @@ using System;
 
 class Dead : PlayerState
 {
+    AnimatedTexture tex;
     double DeathTime = 1;
     public Dead(Player player) : base(player)
     {
         this.player = player;
+        tex = new AnimatedTexture();
+        for(int i = 1; i <= 7; i++)
+            tex.Add(new Texture("./Data/img/player/death/death" + i + ".png"), 0.04);
+    }
+    public override AnimatedTexture GetTexture()
+    {
+        return tex;
+    }
+    public override void Render()
+    {
+        Vec2 pos1 = player.Dir == 1 ? player.Position - player.Size : player.Position - Vec2.CompMult(player.Size, new Vec2(3, 1));
+        Vec2 pos2 = player.Dir == -1 ? player.Position + player.Size : player.Position + Vec2.CompMult(player.Size, new Vec2(3, 1));
+        RenderState.Push();
+        RenderState.Color = player.Color;
+        RenderState.Translate(pos1);
+        RenderState.Scale(pos2 - pos1);
+        if (player.Dir == -1)
+            RenderState.SetOrts(-Vec2.OrtX, Vec2.OrtY, new Vec2(1, 0));
+        GetTexture().Render();
+        RenderState.Pop();
     }
     public override void Update(double dt)
     {
+        if (!tex.HasLooped)
+            tex.Update(dt);
         player.Velocity -= Vec2.Clamp(new Vec2(0, player.Velocity.Y + player.Gravity), player.GAcc * dt);
         bool touch = false;
         if (GetTime() > DeathTime)
@@ -24,11 +47,6 @@ class Dead : PlayerState
                 else touch = true;
         if (touch)
             player.Velocity = Vec2.Zero;
-    }
-
-    public override void Render()
-    {
-        Draw.Rect(player.Position + player.Size, player.Position - player.Size, Color.Gray);
     }
 
     public override void Jump()
