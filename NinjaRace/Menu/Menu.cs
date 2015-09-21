88c394d84dfb2b0f6e771fc4955f456dd69ca975
Shadeoflance <shadeoflance@gmail.com>
2015-09-22 01:20:03 +0300

    Menu keyboard selection

--- NinjaRace/Menu/Menu.cs
+++ NinjaRace/Menu/Menu.cs
@@ -64,71 +64,146 @@ class Menu : UI.State
             Close();
         if (key == Key.Down)
         {
-            KeyboardMode = true;
             SelectNext(-Vec2.OrtY);
+        }
+        if (key == Key.Up)
+        {
+            SelectNext(Vec2.OrtY);
+        }
+        if (key == Key.Left)
+        {
+            SelectNext(-Vec2.OrtX);
+        }
+        if (key == Key.Right)
+        {
+            SelectNext(Vec2.OrtX);
+        }
+        if (key == Key.Enter && Selected != null)
+        {
+            Selected.Click();
         }
     }
 
+    List<UI.Element> Elements = new List<UI.Element>();
     void SelectNext(Vec2 dir)
     {
+        if (Elements.Count == 0)
+        {
+            foreach (var a in Frame.Children)
+                if (a is UI.ElementList)
+                {
+                    foreach (var b in a.Children)
+                        if(!(b is Label))
+                            Elements.Add(b);
+                }
+                else if(!(a is Label)) Elements.Add(a);
+        }
         if (Selected == null)
         {
-            
+            Selected = Elements[0];
+            Selected.Hovered = true;
+            return;
         }
-        double y = Selected.Anchor.Value.Y;
-        UI.Element Next = null;
-        foreach (var a in Frame.Children)
+        Vec2 SelectedVec = Selected.Position;
+        if (dir.X == 1)
         {
-            if (a is Label)
-                continue;
-            Vec2 v;
-            if (a.Anchor == null)
-                continue;
-            else v = a.Anchor.Value;
-            if (Selected == null)
-            {
-                Selected = a;
-                continue;
-            }
-            if (Next == null)
-            {
-                Next = a;
-                continue;
-            }
+            if (IsRight(SelectedVec))
+                SelectedVec = new Vec2(0, SelectedVec.Y);
+            List<UI.Element> t = Elements.FindAll(new Predicate<UI.Element>(Elem => Elem.Position.X > SelectedVec.X));
+            t.Sort(new Comparison<UI.Element>(
+                delegate(UI.Element e1, UI.Element e2) { return e1.Position.X.CompareTo(e2.Position.X); }));
+            t = t.FindAll(new Predicate<UI.Element>(
+                Elem => Elem.Position.X == t[0].Position.X));
+            
+            int result = t.BinarySearch(Selected, Comparer<UI.Element>.Create(
+                delegate(UI.Element e1, UI.Element e2) { return e2.Position.Y.CompareTo(e1.Position.Y); }));
+            if (result == -1)
+                return;
+            Selected.Hovered = false;
+            Selected = t[result];
+            Selected.Hovered = true;
+            return;
+        }
+        if (dir.X == -1)
+        {
+            if (IsLeft(SelectedVec))
+                SelectedVec = new Vec2(App.Width, SelectedVec.Y);
+            List<UI.Element> t = Elements.FindAll(new Predicate<UI.Element>(Elem => Elem.Position.X < SelectedVec.X));
+            t.Sort(new Comparison<UI.Element>(
+                delegate(UI.Element e1, UI.Element e2) { return e2.Position.X.CompareTo(e1.Position.X); }));
+            t = t.FindAll(new Predicate<UI.Element>(
+                Elem => Elem.Position.X == t[0].Position.X));
+
+            int result = t.BinarySearch(Selected, Comparer<UI.Element>.Create(
+                delegate(UI.Element e1, UI.Element e2) { return e2.Position.Y.CompareTo(e1.Position.Y); }));
+            if (result == -1)
+                return;
+            Selected.Hovered = false;
+            Selected = t[result];
+            Selected.Hovered = true;
+            return;
+        }
+        if (dir.Y == 1)
+        {
+            if (IsTop(SelectedVec))
+                SelectedVec = new Vec2(SelectedVec.X, 0);
+            List<UI.Element> t = Elements.FindAll(new Predicate<UI.Element>(Elem => Elem.Position.Y >= SelectedVec.Y &&
+                Elem != Selected &&
+                !(Elem.Position.Y == SelectedVec.Y && Elem.Position.X > SelectedVec.X)));
+            t.Sort(new Comparison<UI.Element>(
+                delegate(UI.Element e1, UI.Element e2) { return e1.Position.Y.CompareTo(e2.Position.Y); }));
+            t = t.FindAll(new Predicate<UI.Element>(
+                Elem => Elem.Position.Y == t[0].Position.Y));
+            t.Sort(new Comparison<UI.Element>(
+                delegate(UI.Element e1, UI.Element e2) { return e2.Position.X.CompareTo(e1.Position.X); }));
+            Selected.Hovered = false;
+            Selected = t[0];
+            Selected.Hovered = true;
+            return;
+        }
+        if (dir.Y == -1)
+        {
+            if (IsBot(SelectedVec))
+                SelectedVec = new Vec2(SelectedVec.X, App.Height);
+            List<UI.Element> t = Elements.FindAll(new Predicate<UI.Element>(Elem => Elem.Position.Y <= SelectedVec.Y &&
+                Elem != Selected &&
+                !(Elem.Position.Y == SelectedVec.Y && Elem.Position.X < SelectedVec.X)));
+            t.Sort(new Comparison<UI.Element>(
+                delegate(UI.Element e1, UI.Element e2) { return e2.Position.Y.CompareTo(e1.Position.Y); }));
+            t = t.FindAll(new Predicate<UI.Element>(
+                Elem => Elem.Position.Y == t[0].Position.Y));
+            t.Sort(new Comparison<UI.Element>(
+                delegate(UI.Element e1, UI.Element e2) { return e1.Position.X.CompareTo(e2.Position.X); }));
+            Selected.Hovered = false;
+            Selected = t[0];
+            Selected.Hovered = true;
+            return;
         }
     }
     bool IsBot(Vec2 p)
     {
-        foreach (var a in Frame.Children)
-        {
-            if (a.Anchor != null && a.Anchor.Value.Y < p.Y)
-                return false;
-        }
-        return true;
+        return Elements.FindAll(new Predicate<UI.Element>(Elem => Elem.Position.Y < p.Y ||
+            (Elem.Position.Y == p.Y && Elem.Position.X > p.X))).Count == 0;
     }
     bool IsTop(Vec2 p)
     {
-        foreach (var a in Frame.Children)
-        {
-            if (a.Anchor != null && a.Anchor.Value.Y > p.Y)
-                return false;
-        }
-        return true;
+        return Elements.FindAll(new Predicate<UI.Element>(Elem => Elem.Position.Y > p.Y ||
+            (Elem.Position.Y == p.Y && Elem.Position.X < p.X))).Count == 0;
     }
     bool IsLeft(Vec2 p)
     {
-        foreach (var a in Frame.Children)
+        foreach (var a in Elements)
         {
-            if (a.Anchor != null && a.Anchor.Value.X < p.X)
+            if (a.Position.X < p.X)
                 return false;
         }
         return true;
     }
     bool IsRight(Vec2 p)
     {
-        foreach (var a in Frame.Children)
+        foreach (var a in Elements)
         {
-            if (a.Anchor != null && a.Anchor.Value.X > p.X)
+            if (a.Position.X > p.X)
                 return false;
         }
         return true;
