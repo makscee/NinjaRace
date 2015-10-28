120f23589b8bbea32441fc1b5239c86a7aeaef9b
Shadeoflance <shadeoflance@gmail.com>
2015-10-28 20:11:35 +0300

    Ground corners add

--- NinjaRace/Level/Tile/impl/Ground.cs
+++ NinjaRace/Level/Tile/impl/Ground.cs
@@ -6,6 +6,7 @@ using VitPro.Engine;
 class Ground : Tile
 {
     int Sides = 15;
+    int Corners = 0;
     public Ground()
     {
         Color = new Color(0.6, 0.8, 0.3);
@@ -19,18 +20,28 @@ class Ground : Tile
             return;
         Vec2i pos = Tiles.GetCoords(ID);
         Sides = 15;
-        if (pos.Y < tiles.GetLength(0) - 1 && tiles.GetTile(pos.X, pos.Y + 1) is Ground)
+        if (tiles.GetTile(pos.X, pos.Y + 1) is Ground)
             Sides -= 1;
-        if (pos.X < tiles.GetLength(1) - 1 && tiles.GetTile(pos.X + 1, pos.Y) is Ground)
+        if (tiles.GetTile(pos.X + 1, pos.Y) is Ground)
             Sides -= 2;
-        if (pos.Y > 1 && tiles.GetTile(pos.X, pos.Y - 1) is Ground)
+        if (tiles.GetTile(pos.X, pos.Y - 1) is Ground)
             Sides -= 4;
-        if (pos.X > 1 && tiles.GetTile(pos.X - 1, pos.Y) is Ground)
+        if (tiles.GetTile(pos.X - 1, pos.Y) is Ground)
             Sides -= 8;
+        Corners = 0;
+        if ((Sides & 1) == 0 && (Sides & 2) == 0 && !(tiles.GetTile(pos.X + 1, pos.Y + 1) is Ground))
+            Corners += 1;
+        if ((Sides & 2) == 0 && (Sides & 4) == 0 && !(tiles.GetTile(pos.X + 1, pos.Y - 1) is Ground))
+            Corners += 2;
+        if ((Sides & 4) == 0 && (Sides & 8) == 0 && !(tiles.GetTile(pos.X - 1, pos.Y - 1) is Ground))
+            Corners += 4;
+        if ((Sides & 8) == 0 && (Sides & 1) == 0 && !(tiles.GetTile(pos.X - 1, pos.Y + 1) is Ground))
+            Corners += 8;
     }
     protected override void SetAdditionalParameters()
     {
         RenderState.Set("sides", Sides);
+        RenderState.Set("corners", Corners);
     }
     public override void Effect(Player player, Side side)
     {
